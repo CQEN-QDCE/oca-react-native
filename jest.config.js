7506b7dbb60cae3886f1400034eefaa1675fbbb5
Jean-Christophe Drouin <jean-christophe.drouin@mcn.gouv.qc.ca>
2022-07-19 10:21:46 -0400

    fix jest setting

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-const { defaults } = require('jest-config');
-module.exports = {
-  preset: 'ts-jest',
-  transform: {
-    '^.+\\.ts?$': 'ts-jest',
-  },
-  transformIgnorePatterns: ['/node_modules/(?!.*react-native.*)'],
-  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
-};
