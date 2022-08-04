627d045322ae1a0e3d4060469096c12a36838b3c
Jean-Christophe Drouin <Jean-Christophe.Drouin@mcn.gouv.qc.ca>
2022-08-04 08:05:53 -0400

    OCA Credential test
    Signed-off-by: Jean-Christophe Drouin <Jean-Christophe.Drouin@mcn.gouv.qc.ca>

--- src/__tests__/components/OCACredential/OcaCredential.test.tsx
+++ src/__tests__/components/OCACredential/OcaCredential.test.tsx
@@ -6,11 +6,37 @@ const attribute = new Map();
 attribute.set('Prénom', 'test');
 
 describe('OcaCredential', () => {
-  it('Minimal OcaCredential props.', async () => {
+  it('Given the minimal OcaCredential props.', async () => {
     let tree;
     await waitFor(
       async () =>
         (tree = render(<OcaCredential attributeValues={attribute} oca={oca} />))
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Given a default language should pass the default language to the webview', async () => {
+    let tree;
+    await waitFor(
+      async () =>
+        (tree = render(
+          <OcaCredential
+            attributeValues={attribute}
+            oca={oca}
+            language={'en'}
+          />
+        ))
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Given a page should pass the page to the webview', async () => {
+    let tree;
+    await waitFor(
+      async () =>
+        (tree = render(
+          <OcaCredential attributeValues={attribute} oca={oca} pageNumber={1} />
+        ))
     );
     expect(tree).toMatchSnapshot();
   });
